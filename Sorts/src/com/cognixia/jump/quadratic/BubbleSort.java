a681d8ded008ccc93a2d51f9f47113562a3006ce
Austin Brooks <austin.williambrooks@gmail.com>
2021-08-18 15:49:43 -0800

    Created a bubble sort class

--- Sorts/src/com/cognixia/jump/quadratic/BubbleSort.java
+++ Sorts/src/com/cognixia/jump/quadratic/BubbleSort.java
@@ -1,5 +1,22 @@
 package com.cognixia.jump.quadratic;
 
 public class BubbleSort {
-
+	
+	public int[] bubble(int[] myArray) {
+		
+		int temp = 0;
+		
+		for(int i = 0; i < myArray.length; i++) {
+			for(int j = 1; j < myArray.length-i; j++) {
+				if(myArray[j-1] > myArray[j]) {			// Check if element is bigger and swap
+					temp = myArray[j-1];
+					myArray[j-1] = myArray[j];
+					myArray[j] = temp;
+				}
+			}
+		}
+		
+		
+		return myArray;
+	}
 }
